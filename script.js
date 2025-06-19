02f1397c942e4da4d60b6382d2912d61f0826b49
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-19 19:32:21 +0300

    finish

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let html = "";
 
 for (let i = 1; i <= 32; i++) {
   html += `<li>
-          <a href="./img/img (${i}).jpg" target="_blank">
-            <img src="./img/img (${i}).jpg" alt="image" />
+          <a href="./img/img(${i}).jpg" target="_blank">
+            <img src="./img/img(${i}).jpg" alt="image" />
           </a>
         </li>`;
 }
